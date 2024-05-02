70f5c5407b21b6c7bf7c1af77b8940a8c11848bc
sankaire <kerenketepela@gmail.com>
2024-05-02 12:02:56 +0300

    Chore:✅ check enviroment on start

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { format, transports } from "winston";
+import env_vars from "./config/env_vars.js";
+import logger from "./utils/logging.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+if (env_vars.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    }),
+  );
+}
 
 app.use("/", (_: Request, res: Response) => {
   return res.json({ links: ["https://theater.ke"] });
