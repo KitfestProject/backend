68b965f7a2937beecfadab588c7085c04bb64421
sankaire <kerenketepela@gmail.com>
2024-05-03 15:16:49 +0300

    Feat:⚓️ jwt methods utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,8 @@
-import { sign, verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { IJwtPayload } from "../../interfaces/index.js";
 import env_vars from "../config/env_vars.js";
+
+const { sign, verify } = jwt;
 
 const create_token = (payload: IJwtPayload): string => {
   return sign(payload, env_vars.JWT_SECRET, {
