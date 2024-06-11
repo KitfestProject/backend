f4b05b60c31da4f365390a0bb6f3aff8a976ffd6
sankaire <kerenketepela@gmail.com>
2024-06-11 20:22:18 +0300

    Feat:register events API

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import user_routes from "../resources/users/user.route.js";
 import category_routes from "../resources/categories/category.route.js";
 import venue_routes from "../resources/venues/venue.route.js";
 import preference_router from "../resources/preferences/prefernce.route.js";
+import events_routes from "../resources/events/events.routes.js";
 
 const api = Router();
 
@@ -10,5 +11,6 @@ api.use("/users", user_routes);
 api.use("/categories", category_routes);
 api.use("/venues", venue_routes);
 api.use("/preference", preference_router);
+api.use("/events", events_routes);
 
 export default api;
