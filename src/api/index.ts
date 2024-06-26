7d95e56ba849d23b91369bde85ae86cce82741f5
sankaire <kerenketepela@gmail.com>
2024-06-26 21:57:34 +0300

    Feat:register blogs API

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import category_routes from "../resources/categories/category.route.js";
 import venue_routes from "../resources/venues/venue.route.js";
 import preference_router from "../resources/preferences/prefernce.route.js";
 import events_routes from "../resources/events/events.routes.js";
+import blog_router from "../resources/blogs/blog.router.js";
 
 const api = Router();
 
@@ -12,5 +13,6 @@ api.use("/categories", category_routes);
 api.use("/venues", venue_routes);
 api.use("/preference", preference_router);
 api.use("/events", events_routes);
+api.use("/blogs", blog_router);
 
 export default api;
