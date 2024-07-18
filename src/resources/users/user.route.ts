c3641fbda6d31c5e4d3c00b2129d19e827169bbc
sankaire <kerenketepela@gmail.com>
2024-07-18 16:52:42 +0300

    Chore:convert users fetch API http verb

--- src/resources/users/user.route.ts
+++ src/resources/users/user.route.ts
@@ -8,7 +8,7 @@ user_routes.post("/sign_up", user_controller.sign_up);
 user_routes.post("/sign_in", user_controller.sign_in);
 user_routes.get("/verify", auth.authenticate, user_controller.verify_user);
 
-user_routes.get(
+user_routes.post(
   "/",
   auth.authenticate,
   auth.authorize_admin,
