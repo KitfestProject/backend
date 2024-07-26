908bb803300ce8682102cc441d8f3256aca7b065
sankaire <kerenketepela@gmail.com>
2024-07-26 12:26:54 +0300

    Chore:fix seatmap

--- src/resources/bookings/booking.service.ts
+++ src/resources/bookings/booking.service.ts
@@ -79,7 +79,7 @@ const book_ticket = async (
     logger.error("Event not found");
     return { success: false, message: "Could not proccess event" };
   }
-  if (tickets.length < 2) {
+  if (seats.length > 0) {
     const seat_ids = seats.map((seat) => seat.id);
     await Sections.updateMany(
       {
