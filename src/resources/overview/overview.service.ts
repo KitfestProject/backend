deb00dfc44d3fea845a90423fb381654805f0c0d
sankaire <kerenketepela@gmail.com>
2024-07-29 14:40:50 +0300

    Chore:return chart data

--- src/resources/overview/overview.service.ts
+++ src/resources/overview/overview.service.ts
@@ -9,6 +9,7 @@ const fetch_stats = async (id: string, is_admin: boolean) => {
   let total_tickets_sold = 0;
   let total_events = 0;
   let total_users = 0;
+  let revenue_per_month = {};
   if (!is_admin) {
     const events = await Events.find({ organizer: id });
     const event_ids = events.map((event) => event._id);
@@ -18,6 +19,32 @@ const fetch_stats = async (id: string, is_admin: boolean) => {
     });
     total_events = events.length;
     total_revenue = tx.reduce((acc, tx) => acc + tx.amount, 0);
+    const today = new Date();
+    const last_month = new Date(today.setMonth(today.getMonth() - 1));
+    const monthly_revenue = new Array(12).fill(0);
+    tx.forEach((transaction) => {
+      if (new Date(transaction.time) > last_month) {
+        monthly_revenue[new Date(transaction.time).getMonth()] +=
+          transaction.amount;
+      }
+    });
+    revenue_per_month = {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      data: monthly_revenue,
+    };
   } else {
     const transactions = await Transactions.find({});
     total_events = await Events.countDocuments({});
@@ -27,6 +54,32 @@ const fetch_stats = async (id: string, is_admin: boolean) => {
       (acc, transaction) => acc + transaction.amount,
       0,
     );
+    const today = new Date();
+    const last_month = new Date(today.setMonth(today.getMonth() - 1));
+    const monthly_revenue = new Array(12).fill(0);
+    transactions.forEach((transaction) => {
+      if (new Date(transaction.time) > last_month) {
+        monthly_revenue[new Date(transaction.time).getMonth()] +=
+          transaction.amount;
+      }
+    });
+    revenue_per_month = {
+      labels: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      data: monthly_revenue,
+    };
   }
 
   const data = {
@@ -34,6 +87,7 @@ const fetch_stats = async (id: string, is_admin: boolean) => {
     total_tickets_sold,
     total_events,
     total_users,
+    revenue_per_month,
   };
 
   return createResponse(true, "Overview stats fetched", data);
