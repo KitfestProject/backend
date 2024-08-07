8a5e96ff7e5b8f4bdd207adbc1f5edfec0ff97c8
sankaire <kerenketepela@gmail.com>
2024-08-07 16:14:08 +0300

    Chore:read port from env

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from "./app.js";
 import env_vars from "./config/env_vars.js";
 import logger from "./utils/logging.js";
 
-app.listen(5100, () =>
+app.listen(env_vars.PORT, () =>
   logger.info(`Api running on port http://localhost:${env_vars.PORT}`),
 );
