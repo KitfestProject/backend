83688cfbe79af0f37200b83b8146e455508f9f1e
sankaire <kerenketepela@gmail.com>
2024-08-19 18:17:28 +0300

    Chore:handle crud utils errors

--- src/utils/crud.ts
+++ src/utils/crud.ts
@@ -4,16 +4,18 @@ import { Document } from "mongoose";
 import createResponse from "./response_envelope.js";
 import { get_current_date_time } from "./date_time.js";
 import collection from "./collection.js";
+import logger from "./logging.js";
 
 const createOne =
   <T extends Document>(model: TModel<T>) =>
   async (req: Request, res: Response) => {
     try {
-      const data = req.body;
+      let data = req.body;
+      data = collection.convert_keys(data);
       const created_at = get_current_date_time();
       data.created_at = created_at;
       data.updated_at = created_at;
-      const doc = await model.create(req.body);
+      const doc = await model.create(data);
       if (!doc) {
         return res
           .status(200)
@@ -22,7 +24,11 @@ const createOne =
       return res
         .status(201)
         .json(createResponse(true, "Created Successfully", doc));
-    } catch (error) {}
+    } catch (error) {
+      const err = error as Error;
+      logger.error(err.message);
+      return res.status(500).end();
+    }
   };
 const getMany =
   <T extends Document>(model: TModel<T>) =>
@@ -37,7 +43,9 @@ const getMany =
         .status(200)
         .json(createResponse(true, "Data fetched Successfully", docs));
     } catch (error) {
-      return res.status(500).json(error);
+      const err = error as Error;
+      logger.error(err.message);
+      return res.status(500).end();
     }
   };
 const getOne =
@@ -55,7 +63,11 @@ const getOne =
       return res
         .status(200)
         .json(createResponse(true, "Data fetched Successfully!", doc));
-    } catch (error) {}
+    } catch (error) {
+      const err = error as Error;
+      logger.error(err.message);
+      return res.status(500).end();
+    }
   };
 const updateOne =
   <T extends Document>(model: TModel<T>) =>
@@ -77,7 +89,11 @@ const updateOne =
       return res
         .status(200)
         .json(createResponse(true, "Updated Successfully", doc));
-    } catch (error) {}
+    } catch (error) {
+      const err = error as Error;
+      logger.error(err.message);
+      return res.status(500).end();
+    }
   };
 const deleteOne =
   <T extends Document>(model: TModel<T>) =>
@@ -93,7 +109,11 @@ const deleteOne =
       return res
         .status(200)
         .json(createResponse(true, "Deleted Successfully", doc));
-    } catch (error) {}
+    } catch (error) {
+      const err = error as Error;
+      logger.error(err.message);
+      return res.status(500).end();
+    }
   };
 
 export default { createOne, getMany, getOne, updateOne, deleteOne };
