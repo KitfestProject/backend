6fe39db561daf245a7be1aea31e7fd4433a74b4c
sankaire <kerenketepela@gmail.com>
2024-08-20 14:02:59 +0300

    Fix:update section query

--- src/resources/seatmaps/seatmap.service.ts
+++ src/resources/seatmaps/seatmap.service.ts
@@ -81,10 +81,10 @@ const fetch_section_by_abbr_name = async (
   }
 };
 const update_section = async (id: string, data: ISections) => {
-  const section = await Sections.findOneAndUpdate(
-    { _id: id },
-    { data, returnDocument: "after" },
-  );
+  const section = await Sections.findOneAndUpdate({ _id: id }, data, {
+    returnDocument: "after",
+  });
+  console.log(section);
   if (!section) {
     return createResponse(false, "Failed to update section", null);
   }
@@ -172,24 +172,23 @@ const fetch_sections = async (event_id: string) => {
 };
 const update_section_seats = async (section_id: string, seat: ISeats) => {
   // console.log(seats);
-  const result = await Sections.findOneAndUpdate(
+  const result = await Sections.updateOne(
     {
       _id: section_id,
       "rows.seats._id": seat._id,
     },
     {
       $set: {
-        seat,
+        "rows.$[].seats.$[seat]": seat,
       },
     },
     {
-      returnDocument: "after",
+      arrayFilters: [{ "seat._id": seat._id }],
     },
   );
-  console.log(result);
-  // if (result === 0) {
-  //   return createResponse(false, "Seats not updated", null);
-  // }
+  if (result.matchedCount < 0) {
+    return createResponse(false, "Failed to update seat.", null);
+  }
   return createResponse(true, "Seats updated successfully", null);
 };
 function count_section_seats(section: ISections) {
