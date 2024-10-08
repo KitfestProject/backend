c70cb1bc7e01ec494fcde62485301469e18ae361
sankaire <kerenketepela@gmail.com>
2024-10-08 15:27:56 +0300

    Chore:provide excecutable

--- src/resources/bookings/booking.service.ts
+++ src/resources/bookings/booking.service.ts
@@ -315,6 +315,7 @@ async function generate_ticket_pdf(
     const templatePath = path.join(__dirname, "public", "e-ticket.ejs");
     const html = await ejs.renderFile(templatePath, pdf_data);
     const browser = await puppeteer.launch({
+      executablePath: "/usr/bin/chromium",
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
